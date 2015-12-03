4196c0d9e6489874a1a6615b4cf4371a9cd2ae08
f-konashi <f-konashi@keyportsolutions.com>
2015-12-03 13:50:41 +0900

    drop_table.sqlに、DROPシークエンス文を追記

--- drop_table.sql
+++ drop_table.sql
@@ -5,3 +5,11 @@ DROP TABLE buying_history;
 DROP TABLE item_info;
 DROP TABLE user_authority;
 DROP TABLE user_info;
+
+-- ************************************
+-- シーケンス削除
+-- ************************************
+
+DROP SEQUENCE BuyingID_seq;
+DROP SEQUENCE ItemID_seq;
+DROP SEQUENCE UserID_seq;
